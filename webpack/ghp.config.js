51c23a6faae66f13c85fa2f369df46a8f68ece84
Gabriela Vazquez <gabs.vz@gmail.com>
2016-10-17 18:18:28 -0700

    Trying github pages content from docs folder.

--- webpack/ghp.config.js
+++ webpack/ghp.config.js
@@ -1,16 +1,11 @@
 /* global __dirname */
 var webpack = require("webpack");
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = exports = require("./base.config.js");
 
 exports.entry.app = ['./demo/main.ts'];
 
 exports.plugins = [
-    new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: './demo/ghp-index.html'
-    }),
     new webpack.optimize.CommonsChunkPlugin(
         /* chunkName= */'vendor', /* filename= */'bundles/vendor.bundle.js'
     )
